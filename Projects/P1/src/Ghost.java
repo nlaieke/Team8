10c98d1f44bcabebbd63b8b7cb971fa62fbfdd69
Alex-Srey <66026986+Alex-Srey@users.noreply.github.com>
2022-01-06 12:56:30 -0500

    Add files via upload
    not yet tested, added is_pacman_in_range

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -21,7 +21,21 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
+		ArrayList<Location> pacmanLocations = new ArrayList<Location>();
+		pacmanLocations.add(new Location(myLoc.x + 1, MyLoc.y + 0));
+		pacmanLocations.add(new Location(myLoc.x + 0, MyLoc.y + 1));
+		pacmanLocations.add(new Location(myLoc.x + 0, MyLoc.y - 1));
+		pacmanLocations.add(new Location(myLoc.x - 1, MyLoc.y + 0));
+		
+		
+		for(Location location: pacmanLocations){
+			Map.Map.Type pacmanCheck = myMap.getLoc(location);
+			if(pacmanCheck == Map.Type.PACMAN){
+				return true;
+			}
+	}
 		return false;
+	}
 	}
 
 	public boolean attack() {
