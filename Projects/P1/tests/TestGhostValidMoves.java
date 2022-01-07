0c2128044a512796a51e9dc0ab97b0498eb6f24e
Connor Hartzog <hartzogcr@gmail.com>
2022-01-07 14:25:31 -0500

    more tests

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -7,12 +7,26 @@ import com.sun.jdi.Location;
 
 public class TestGhostValidMoves extends TestCase {
 
-	public void testGhostValidMoves() throws FileNotFoundException{
+	public void testGhostValidMovesEmpty() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
 		ArrayList<Location> validMoves = new ArrayList<Location>();
 		validMoves.add(new Location(9,12));
 		validMoves.add(new Location(10,11));
 		Ghost ghost = frame.addGhost(new Location(9,11), "clyde", Color.orange);
+		System.out.print(validMoves.equals(ghost.get_valid_moves()));
+		assertTrue(validMoves.equals(ghost.get_valid_moves())); // equal ArrayLists.  
+		
+	}
+	public void testGhostValidMovesGhostsAndPacMan() throws FileNotFoundException{
+		NoFrame frame = new NoFrame();
+		ArrayList<Location> validMoves = new ArrayList<Location>();
+		validMoves.add(new Location(9,12));
+		validMoves.add(new Location(10,11));
+		Ghost ghost = frame.addGhost(new Location(9,11), "clyde", Color.orange);
+		Ghost testGhost = frame.addGhost(new Location(9,11), "blinky", Color.red);
+
+		PacMan pacman = frame.addPacMan(new Location (9,12));
+
 		System.out.print(validMoves.equals(ghost.get_valid_moves()));
 		assertTrue(validMoves.equals(ghost.get_valid_moves())); // equal ArrayLists.  
 		
