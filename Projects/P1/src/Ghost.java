e26351fb2192e34eb15f4180c27b2bebfda8edb0
austin <austinkim1004@gmail.com>
2022-01-07 23:33:16 -0500

    finished ghost attack()

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -25,11 +25,16 @@ public class Ghost{
 	}
 
 	public boolean attack() {
-		if (is_pacman_in_range) {
-			
-			return true
+		// check if in range to attack
+		if (is_pacman_in_range()) {
+			// attempt ghost attack using the map class method
+			if (myMap.attack(myName)) {
+				// success
+				return true;
+			}
 		}
 		
-		return false
+		// failure
+		return false;
 	}
 }
