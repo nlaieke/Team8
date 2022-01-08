7d6364c5627e1a71e1c8d97853885b29e0ed2022
Alex-Srey <66026986+Alex-Srey@users.noreply.github.com>
2022-01-07 19:07:59 -0500

    tests for map attack

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,9 +1,22 @@
 import java.awt.Color;
 import junit.framework.*;
+import java.io.*;
 
-public class TestMapAttack {
+public class TestMapAttack extends TestCase {
 
-	public void testMapAttack() {
-		return null;	
+	public void testMapAttack() throws FileNotFoundException {
+		//ghost is in range and attacks, checks to see if gameover was returned
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(8, 7), "clyde", Color.red);
+		Map map = frame.getMap();		//Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(7, 7));
+		assertTrue(map.attack("clyde") == false);
+		
+		//ghost is out of range and cannot attack so game continues
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(10, 7), "clyde", Color.red);
+		Map map = frame.getMap();		//Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(7, 7));
+		assertTrue(map.attack("clyde") == true);
 	}
 }
