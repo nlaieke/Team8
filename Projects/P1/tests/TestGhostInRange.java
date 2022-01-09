ebadeb2296429fc48cceb00083625aaf7df174d6
Alex-Srey <66026986+Alex-Srey@users.noreply.github.com>
2022-01-09 12:19:17 -0500

    Add files via upload
    tests ghost in range and out of range

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -5,7 +5,21 @@ import java.io.*;
 public class TestGhostInRange extends TestCase {
 
 	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(8, 7), "clyde", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(7, 7));
+		assertTrue(pacman.is_ghost_in_range());
+	
+		
+	}
+	
+	public void testGhostInRange2() throws FileNotFoundException{
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(8, 8), "clyde", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(7, 7));
+		//Should fail
+		assertTrue(pacman.is_ghost_in_range());
+		
 	}
 }
 
