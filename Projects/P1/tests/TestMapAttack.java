435da3062d6818d762ee59dd437eadcf22b43d93
Alex-Srey <66026986+Alex-Srey@users.noreply.github.com>
2022-01-09 12:25:08 -0500

    fixed errors in testing

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -10,13 +10,16 @@ public class TestMapAttack extends TestCase {
 		Ghost ghost = frame.addGhost(new Location(8, 7), "clyde", Color.red);
 		Map map = frame.getMap();		//Creates a red ghost named "name" at location x,y
 		PacMan pacman = frame.addPacMan(new Location(7, 7));
+		//game ends, gameover is returned
 		assertTrue(map.attack("clyde") == false);
-		
+	}
+	public void testMapAttack2() throws FileNotFoundException {
 		//ghost is out of range and cannot attack so game continues
 		NoFrame frame = new NoFrame();
 		Ghost ghost = frame.addGhost(new Location(10, 7), "clyde", Color.red);
 		Map map = frame.getMap();		//Creates a red ghost named "name" at location x,y
 		PacMan pacman = frame.addPacMan(new Location(7, 7));
+		//because game continues, map.attack does not return gameover (false)
 		assertTrue(map.attack("clyde") == true);
 	}
 }
