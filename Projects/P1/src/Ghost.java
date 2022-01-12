27f7a7ab34e5aef520beaba34cfbd2bacba69995
austin <austinkim1004@gmail.com>
2022-01-12 16:17:00 -0500

    sabotaged ghost attack

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -63,7 +63,7 @@ public class Ghost{
 
 	public boolean attack() {
 		// check if in range to attack
-		if (is_pacman_in_range()) {
+		if (is_pacman_in_range() == false) {
 			// attempt ghost attack using the map class method
 			if (myMap.attack(myName)) {
 				// success
