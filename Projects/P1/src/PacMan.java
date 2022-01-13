9d1f11dde62b675192d0bcc9c399841bdf64d6c3
Alex-Srey <66026986+Alex-Srey@users.noreply.github.com>
2022-01-12 19:11:46 -0500

    sabotage pacman (is_ghost_in_range)

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -48,6 +48,7 @@ public class PacMan{
 		ghostLocations.add(new Location(this.myLoc.x + 1, this.myLoc.y + 0));
 		ghostLocations.add(new Location(this.myLoc.x + 0, this.myLoc.y + 1));
 		ghostLocations.add(new Location(this.myLoc.x + 0, this.myLoc.y - 1));
+		ghostLocations.add(new Location(this.myLoc.x - 1, this.myLoc.y - 1));
 		ghostLocations.add(new Location(this.myLoc.x - 1, this.myLoc.y + 0));
 		
 		
